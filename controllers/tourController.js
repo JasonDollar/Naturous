53498573a32cfb3af6cecb5f6d9aada27901e360
JasonDollar <elrey33@gmail.com>
2019-06-27 12:16:05 +0200

    extended Tour model, imrovements t o controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,33 +7,47 @@ const Tour = require('../models/Tour')
 
 
 exports.getAllTours = async (req, res) => {
-  const tours = await Tour.find()
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.time,
-    results: tours.length,
-    data: {
-      tours
-    }
-  })
+  try {
+    const tours = await Tour.find()
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    })
+  } catch(e) {
+    return res.status(404).json({
+      status: 'error',
+      message: e.message
+    })
+  }
 }
 
 exports.getTour = async (req, res) => {
-  const tour = await Tour.findById(req.params.id)
-  if (!tour) {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    if (!tour) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID'
+      })
+    }
+  
+    res.status(200).json({
+      status: 'success',
+      // results: tours.length,
+      data: {
+        tour
+      }
+    })
+
+  } catch(e) {
     return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
+      status: 'error',
+      message: e.message
     })
   }
-
-  res.status(200).json({
-    status: 'success',
-    // results: tours.length,
-    data: {
-      tour
-    }
-  })
 }
 
 exports.createTour = async (req, res) => {
@@ -57,24 +71,44 @@ exports.createTour = async (req, res) => {
   }
 }
 
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+      // new zwroci udated document
+    })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    })
+  } catch (e) {
+
+    res.status(404).json({
+      status: 'error',
+      message: e.message
+    })
+  }
   // const tour = tours.find(item => item.id === req.params.id)
   // const newTour = { ...tour, ...req.body }
   // console.log(newTour.duration)
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour: newTour
-  //   }
-  // })
-  res.status(404).json()
+  
 }
 
-exports.deleteTour = (req, res) => {
-  console.log(req)
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+      status: 'success',
+      data: null
+    })
+  } catch (e) {
 
-  res.status(204).json({
-    status: 'success',
-    data: null
-  })
+    res.status(404).json({
+      status: 'error',
+      message: e.message
+    })
+  }
 }
--- models/Tour.js
+++ models/Tour.js
@@ -3,17 +3,55 @@ const { Schema, model } = require('mongoose')
 const tourSchema = new Schema({
   name: {
     type: String,
+    trim: true,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.5
+    required: [true, 'A tour must have duration'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price']
-  }
+    required: [true, 'A tour must have a price'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: ['easy', 'medium', 'difficult'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 })
 
 const Tour = model('Tour', tourSchema)
