561f080cf12d8efb6d57493828f44f54d392a9ae
JasonDollar <elrey33@gmail.com>
2019-07-01 10:01:48 +0200

    globalErrorHandler created

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
-// const Tour = require('./models/Tour')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 
 const app = express()
 
@@ -21,10 +22,16 @@ app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cant't find ${req.originalUrl} on this server`,
-  })
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Cant't find ${req.originalUrl} on this server`,
+  // })
+  const err = new AppError(`Cant't find ${req.originalUrl} on this server`, 404)
+  // err.status = 'fail'
+  // err.statusCode = 404
+  next(err)
 })
+
+app.use(globalErrorHandler)
 
 module.exports = app
