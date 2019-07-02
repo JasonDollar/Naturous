2285d2dc9e4966c077d795c4eaacaab9aaa72684
JasonDollar <elrey33@gmail.com>
2019-07-02 12:34:40 +0200

    error handling for production

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED EXCEPTION! Shutting down gracefully...')
+  console.log(err)
+  process.exit(1)
+})
+
 dotenv.config({path: './config.env'})
 const app = require('./app')
 
 mongoose.connect(process.env.MONGO_URI_DEV, { useNewUrlParser: true })
   .then(() => console.log('db connected'))
-  .catch(e => console.log(e))
+  .catch(e => console.log('db error', e))
 
   
 
-app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log('server stared')
 })
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down gracefully...')
+  console.log(err)
+  server.close(() => {
+    process.exit(1)
+  })
+})
+
