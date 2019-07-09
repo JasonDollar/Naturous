38f0584d8e73e3c00ad3917cb266a27d12eb78b4
JasonDollar <elrey33@gmail.com>
2019-07-09 20:30:28 +0200

    data sanitization

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 
 const app = express()
-
 if (process.env.NODE_ENV === 'development') {
+  // dev logging
   app.use(morgan('dev'))
 }
-app.use(express.json())
+
+// set security http headers
+app.use(helmet())
+
+// limit requests from the same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour' 
+})
+
+app.use('/api', limiter)
+
+// body parser, data from body to req.body
+app.use(express.json({ limit: '10kb' }))
+
+// data sanitization against noSQL query injection
+app.use(mongoSanitize())
+
+// data sanitization against XSS
+app.use(xss())
+
+// serving static files
 app.use(express.static(`${__dirname}/public`))
 
+// test middleware
 app.use((req, res, next) => {
   req.time = new Date().toISOString()
   next()
