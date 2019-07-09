e3c88644e644ad5a594dee5e5189e5a7ad8311ec
JasonDollar <elrey33@gmail.com>
2019-07-09 20:10:03 +0200

    sending cookie to client

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,8 +12,16 @@ const signToken = id =>  jwt.sign({ id }, process.env.JWT_SECRET, {
 
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id)
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  }
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
 
-  delete user.password
+  user.password = undefined
+
+  res.cookie('jwt', token, cookieOptions)
+
   res.status(statusCode).json({
     status: 'success',
     token,
