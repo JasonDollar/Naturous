4c0f1758bd852bd8946bbbece26e6c24a4c9308a
JasonDollar <elrey33@gmail.com>
2019-07-10 10:14:08 +0200

    review model

--- models/Review.js
+++ models/Review.js
@@ -2,7 +2,31 @@ const { Schema, model } = require('mongoose')
 // const slugify = require('slugify')
 // const validator = require('validator')
 
-const reviewSchema = new Schema ({})
+const reviewSchema = new Schema ({
+  review: {
+    type: String,
+    required: [true, 'Review cannot be empty!'],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  tour: {
+    type: Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review must belong to a tour'],
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to an user'],
+  },
+})
 
 const Review = model('Review', reviewSchema)
 
