d216e90082e3d27e2934c3b3dd9018cfb94ef6bf
JasonDollar <elrey33@gmail.com>
2019-07-11 21:13:18 +0200

    better route protection

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,17 +5,18 @@ const authController = require('../controllers/authController')
 // mergeParams has access to params from other routers
 const router = express.Router({ mergeParams: true })
 
+router.use(authController.protect)
+
 router.route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'), 
     reviewController.setTourUserIds,
     reviewController.createReview)
 
 router.route('/:id')
   .get(reviewController.getReview)
-  .delete(reviewController.deleteReview)
-  .patch(reviewController.updateReview)
+  .delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview)
+  .patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,16 +4,22 @@ const authController = require('../controllers/authController')
 
 const router = express.Router()
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser)
+
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
 
+router.post('/forgotPassword', authController.forgotPassword)
+router.patch('/resetPassword/:token', authController.resetPassword)
+
+// authController.protect will be used for all routes below this declaration
+router.use(authController.protect)
+
+router.patch('/updateMyPassword', authController.updatePassword)
+router.get('/me', userController.getMe, userController.getUser)
 router.post('/updateMe', authController.protect, userController.updateMe)
 router.delete('/deleteMe', authController.protect, userController.deleteMe)
 
-router.post('/forgotPassword', authController.forgotPassword)
-router.patch('/resetPassword/:token', authController.resetPassword)
-router.patch('/updateMyPassword',authController.protect, authController.updatePassword)
+router.use(authController.restrictTo('admin'))
 
 router
   .route('/')
