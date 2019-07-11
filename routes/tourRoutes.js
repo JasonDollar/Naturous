905c721083fd34f36750b0be4185cea2a0e96f64
JasonDollar <elrey33@gmail.com>
2019-07-11 12:48:49 +0200

    merged reviews routes with tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const tourController = require('../controllers/tourController')
 const authController = require('../controllers/authController')
-const reviewController = require('../controllers/reviewController')
+const reviewRouter = require('./reviewRoutes')
 
 const router = express.Router()
+
+router.use('/:tourId/reviews', reviewRouter)
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
 
@@ -21,10 +23,10 @@ router
   .patch(tourController.updateTour)
   .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour)
 
-router.route('/:tourId/reviews').post(
-  authController.protect, 
-  authController.restrictTo('user'), 
-  reviewController.createReview,
-)
+// router.route('/:tourId/reviews').post(
+//   authController.protect, 
+//   authController.restrictTo('user'), 
+//   reviewController.createReview,
+// )
 
 module.exports = router
